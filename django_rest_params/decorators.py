6247f3ebc5e2629138bf3c8ee6b4ac44eafd6ece
Marco Pincheira <marco.pincheira.s@gmail.com>
2019-01-15 16:01:36 -0300

    fixing unicode casting on string on python version 3

--- django_rest_params/decorators.py
+++ django_rest_params/decorators.py
@@ -70,7 +70,10 @@ def params(**kwargs):
                     param = float(param)
                 elif self.param_type == str:
                     assert(isinstance(param, (str, unicode)))
-                    param = unicode(param)
+                    if (sys.version_info > (3, 0)):
+                        param = str(param)
+                    else:
+                        param = unicode(param)
                 elif self.param_type == bool:
                     param = str(param).lower()  # bool isn't case sensitive
                     if param in TRUE_VALUES:
